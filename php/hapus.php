7e68fc7d845973d941a38c9fb10033add8c560b2
sean-vinz <srfvinz@gmail.com>
2026-06-06 19:30:25 +0700

    Delete php directory

--- php/hapus.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'koneksi.php';
-$id = $_GET['id'];
-mysqli_query($koneksi, "DELETE FROM destinasi WHERE id='$id'");
-header("location:index.php");
-?>
--- php/proses_tambah.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'koneksi.php';
-$nama_tempat = $_POST['nama_tempat'];
-$lokasi = $_POST['lokasi'];
-$tiket_masuk = $_POST['tiket_masuk'];
-
-mysqli_query($koneksi, "INSERT INTO destinasi VALUES ('','$nama_tempat','$lokasi','$tiket_masuk')");
-header("location:index.php");
-?>
